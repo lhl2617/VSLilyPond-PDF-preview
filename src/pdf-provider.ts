b70c15011673b1db03c75462fd472b83750fce4e
lhl2617 <lhlee888@hotmail.com>
2021-05-11 22:23:13 +0100

    Working baseline

---
+++ src/pdf-provider.ts
@@ -0,0 +1,48 @@
+import * as vscode from "vscode"
+import { PdfPreview } from "./pdf-preview"
+
+export class PdfCustomProvider implements vscode.CustomReadonlyEditorProvider {
+  public static readonly viewType = "lilypond.pdf.preview"
+
+  private readonly _previews = new Set<PdfPreview>()
+  private _activePreview: PdfPreview | undefined
+
+  constructor(private readonly extensionRoot: vscode.Uri) {}
+
+  public openCustomDocument(uri: vscode.Uri): vscode.CustomDocument {
+    return { uri, dispose: (): void => {} }
+  }
+
+  public async resolveCustomEditor(
+    document: vscode.CustomDocument,
+    webviewEditor: vscode.WebviewPanel
+  ): Promise<void> {
+    const preview = new PdfPreview(
+      this.extensionRoot,
+      document.uri,
+      webviewEditor
+    )
+    this._previews.add(preview)
+    this.setActivePreview(preview)
+
+    webviewEditor.onDidDispose(() => {
+      this._previews.delete(preview)
+    })
+
+    webviewEditor.onDidChangeViewState(() => {
+      if (webviewEditor.active) {
+        this.setActivePreview(preview)
+      } else if (this._activePreview === preview && !webviewEditor.active) {
+        this.setActivePreview(undefined)
+      }
+    })
+  }
+
+  public get activePreview(): PdfPreview | undefined {
+    return this._activePreview
+  }
+
+  private setActivePreview(value: PdfPreview | undefined): void {
+    this._activePreview = value
+  }
+}
